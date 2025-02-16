7a2c6498e34dd4d394af6aefac530ce608b784d3
daadhaymour <135389387+daadhaymour@users.noreply.github.com>
2025-02-16 09:36:33 -0700

    added navbar

--- calgaryhacks2025/app/(tabs)/_layout.tsx
+++ calgaryhacks2025/app/(tabs)/_layout.tsx
@@ -8,36 +8,54 @@ import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+    screenOptions={{
+      tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      headerShown: false,
+      tabBarButton: HapticTab,
+      tabBarBackground: TabBarBackground,
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        position: 'absolute',
+        backgroundColor: '#8FB0C4', 
+        borderRadius: 30,
+        marginHorizontal: 20,
+        marginBottom: 10, 
+        height: 65, 
+        borderTopWidth: 0,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        shadowOffset: { width: 0, height: 5 },
+      },
       }}>
       <Tabs.Screen
-        name="parkselection"
+        name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="user.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="compass.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="parkselection"
+        options={{
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="world.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="2"
+        options={{
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="cart.fill" color={color} />,
         }}
       />
     </Tabs>
